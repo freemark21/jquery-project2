622e4ace9f81aa402067606eda1cefa5b7064138
Ryan Freemark <freemark21@gmail.com>
2016-03-23 17:08:58 -0500

    redo

--- js/script.js
+++ js/script.js
@@ -1,26 +1,27 @@
-$(function () {
-
-    function loadJSON() {
-        $.getJSON('js/info.json')
-            .done(function (data) {
-                $('#portfolio').append('<h1>Portfolio</h1>');
-                for (var i = 0; i < data.portfolio.length; i++) {
-                    $('#portfolio').append(data.portfolio[i].name);
-                }
-            }).fail(function () {
-                $('#skills').append('<h1>Sorry, something went wrong</h1>');
-            });
+$(function() {
+    function loadJSON(){
+    $.getJSON('js/info.json')
+    .done(function (data){
+        for (var i = 0; i < data.things.length; i++){
+            addContent(data.things[i]);
+        };
+        $('img').hide();
+        $('h1').on('click', function(){
+            $(this).siblings().slideToggle();
+        });
+        
+    });
     };
+
+function addContent(obj){
+    $('body').append('<div><h1>' + obj.name + '</h1><img src ="' + obj.img + '"/></div>');
+};
+
+function addFavs(obj){
+    
+}
+
 loadJSON();
 
-    function addSkills() {
-        $('<h1>Skills</h1>').appendTo("#skills");
-        for (var i = 0; i < 5; i++) {
-            if (i % 2 !== 0) {
-                $('#skills').append('<div class="row"><p class="col-md-6">' + 'JSON data' + '</p></div>');
-            } else {
-                $('#skills .row').append('<p class="col-md-6">' + 'JSON data' + '</p>');
-            }
-        };
-    };
+
 });
